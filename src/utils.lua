6d0cc81a1cb60b5ac44b9498e3bfffc1b6d742d6
danameisnothing <123193945+danameisnothing@users.noreply.github.com>
2023-02-06 09:12:27 +0000

    changed to dot

--- src/utils.lua
+++ src/utils.lua
@@ -3,7 +3,7 @@ local utils = {}
 local start = os.time() + 1
 
 -- Returns true if the number is positive, and false if it's not
-function utils:IsPositive(num)
+function utils.IsPositive(num)
 	if num then
         if type(num) ~= "number" then
             error("Argument received is not the type 'number'")
@@ -22,7 +22,7 @@ function utils:IsPositive(num)
 end
 
 -- Returns the biggest number in a table
-function utils:Biggest(table)
+function utils.Biggest(table)
     local biggestNum = nil
     if type(table) ~= "table" then
         error("Argument received is not the type 'table'")
@@ -45,7 +45,7 @@ function utils:Biggest(table)
 end
 
 -- Returns the smallest number in a table
-function utils:Smallest(table)
+function utils.Smallest(table)
     local smallestNum = nil
     if type(table) ~= "table" then
         error("Argument received is not the type 'table'")
@@ -68,18 +68,18 @@ function utils:Smallest(table)
 end
 
 -- Rounds the number to the nearest whole number
-function utils:Round(num)
+function utils.Round(num)
     if type(num) ~= "number" then
         error("Argument received is not the type 'number'")
     end
 
-    if utils:IsPositive(num) then
+    if utils.IsPositive(num) then
         if num - math.floor(num) < 0.5 then
             return math.floor(num)
         else
             return math.ceil(num)
         end
-    elseif not utils:IsPositive(num) then
+    elseif not utils.IsPositive(num) then
         if (num * -1) + math.ceil(num) > 0.5 then
             return math.floor(num)
         else
@@ -91,24 +91,24 @@ function utils:Round(num)
 end
 
 -- Returns the number in positive form
-function utils:ToPositive(num)
+function utils.ToPositive(num)
     if type(num) ~= "number" then
         error("Argument received is not the type 'number'")
     end
 
-    if not utils:IsPositive(num) then
+    if not utils.IsPositive(num) then
         return num * -1
     end
     return num
 end
 
 -- Returns the approximate amount of time in seconds since the unix epoch according to this device's start time and the cpu time used by the program
-function utils:GetApproxTick()
+function utils.GetApproxTick()
     return start + os.clock()
 end
 
 -- Yields the current thread until the specified amount of time in seconds have elapsed
-function utils:Wait(second)
+function utils.Wait(second)
     if second ~= nil and second > 0 then
         if type(second) ~= "number" then
             error("Argument received is not the type 'number'")
@@ -126,24 +126,8 @@ function utils:Wait(second)
     end
 end
 
--- Deprecated
-function utils:ToFormattedHour(num)
-    warn("Broken maybe")
-    if type(num) ~= "number" then
-        error("Argument received is not the type 'number'")
-    end
-
-    if utils:ToPositive(num) then
-        if num >= 60 then
-            return math.floor(num) .. ":" .. math
-        end
-    else
-        error("Unimplemented")
-    end
-end
-
 -- Prints the contents of a table
-function utils:DebugPrintTable(table)
+function utils.DebugPrintTable(table)
     if type(table) ~= "table" then
         error("Argument received is not the type 'table'")
     end
@@ -155,7 +139,7 @@ function utils:DebugPrintTable(table)
 end
 
 -- Truncates the string into a table, Help from : https://stackoverflow.com/questions/41463421/lua-split-string-by-semicolon
-function utils:TruncateSpace(str)
+function utils.TruncateSpace(str)
     if type(str) ~= "string" then
         error("Argument received is not the type 'string'")
     end
